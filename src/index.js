291a12829dd50a6576f8add9b066f09cbeb2a8f6
Shin Jaemin <tlswoals87@naver.com>
2026-02-11 17:21:25 +0900

    26-02-11 학습 내용 저장

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+import reportWebVitals from './reportWebVitals';
+// import Clock from './chapter_04/Clock';
+//import CommentList from './chapter_05/CommentList';
+import NotificationList from  './chapter_06/NotificationList';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+
 root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <NotificationList />
+  </React.StrictMode>,
+  document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
